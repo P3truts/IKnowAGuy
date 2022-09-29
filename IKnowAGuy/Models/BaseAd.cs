2934f76903b06a701278e84a057023762f8455c3
P3truts <obreja.petrut@gmail.com>
2022-09-29 11:42:02 +0300

    Models: change Address to AddressId

--- IKnowAGuy/Models/BaseAd.cs
+++ IKnowAGuy/Models/BaseAd.cs
@@ -3,15 +3,15 @@
     public abstract class BaseAd : Base
     {
         public DateTime Date { get; private set; }
-        public string Address { get; private set; }
+        public int AddressId { get; private set; }
         public int JobId { get; private set; }
         public int ServiceId { get; private set; }
-        protected BaseAd(int id, string name, string description, DateTime date, string address, int jobId, int serviceId) 
+        protected BaseAd(int id, string name, string description, DateTime date, int addressId, int jobId, int serviceId) 
             : base(id, name, description)
         {
             JobId = jobId;
             ServiceId = serviceId;
-            Address = address;
+            AddressId = addressId;
             Date = date;
         }
     }
