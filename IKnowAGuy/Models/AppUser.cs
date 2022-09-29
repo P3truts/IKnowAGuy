a6ec597bf967b05b564a5151107a55526b814e58
P3truts <obreja.petrut@gmail.com>
2022-09-29 18:06:48 +0300

    Database: add AppUser model

---
+++ IKnowAGuy/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IKnowAGuy.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string? AdId { get; set; }
+        public List<Job>? Jobs { get; set; }
+        public List<Service>? Services { get; set; }
+        public string? CompanyType { get; set; }
+        public string? CompanyName { get; set; }
+        public string? CompanyDescription { get; set; }
+    }
+}
