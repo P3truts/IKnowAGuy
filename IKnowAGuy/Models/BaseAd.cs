e6f1a446b776902d6e89a54e003996fcac17acc2
P3truts <obreja.petrut@gmail.com>
2022-09-29 10:58:24 +0300

    Models: add BaseAd class

---
+++ IKnowAGuy/Models/BaseAd.cs
@@ -0,0 +1,18 @@
+﻿namespace IKnowAGuy.Models
+{
+    public abstract class BaseAd : Base
+    {
+        public DateTime Date { get; private set; }
+        public string Address { get; private set; }
+        public int JobId { get; private set; }
+        public int ServiceId { get; private set; }
+        protected BaseAd(int id, string name, string description, DateTime date, string address, int jobId, int serviceId) 
+            : base(id, name, description)
+        {
+            JobId = jobId;
+            ServiceId = serviceId;
+            Address = address;
+            Date = date;
+        }
+    }
+}
