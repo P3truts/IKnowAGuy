169a4b63f0347156be610456816fb862f30b55f1
P3truts <obreja.petrut@gmail.com>
2022-09-29 18:06:32 +0300

    Database: Address model - del constr, change Id

--- IKnowAGuy/Models/Address.cs
+++ IKnowAGuy/Models/Address.cs
@@ -2,15 +2,8 @@
 {
     public class Address
     {
-        public int AddressId { get; private set; }
-        public string County { get; private set; }
-        public string City { get; private set; }
-
-        public Address(int addressId, string county, string city)
-        {
-            AddressId = addressId;
-            County = county;
-            City = city;
-        }
+        public string? AddressId { get; private set; }
+        public string? County { get; private set; }
+        public string? City { get; private set; }
     }
 }
