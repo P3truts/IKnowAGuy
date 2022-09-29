883d5299f39cc7f5cadfbfcac8ca76acb80f6371
P3truts <obreja.petrut@gmail.com>
2022-09-29 11:13:04 +0300

    Models: add Service model

---
+++ IKnowAGuy/Models/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace IKnowAGuy.Models
+{
+    public class Service : Base
+    {
+        public int ServiceId { get; private set; }
+        public Service(int id, string name, string description, int serviceId) : base(id, name, description)
+        {
+            ServiceId = serviceId;
+        }
+    }
+}
