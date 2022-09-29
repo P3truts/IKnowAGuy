60203120ef741520e828cf4138103418ad4f0ee5
P3truts <obreja.petrut@gmail.com>
2022-09-29 18:09:42 +0300

    Database: rename BaseAd to Ad, del constr, change props

---
+++ IKnowAGuy/Models/Ad.cs
@@ -0,0 +1,12 @@
+﻿namespace IKnowAGuy.Models
+{
+    public class Ad : Base
+    {
+        public DateTime Date { get; private set; }
+        public string? AddressId { get; private set; }
+        public string? JobId { get; private set; }
+        public string? ServiceId { get; private set; }
+        public string? UserId { get; private set; }
+        public string? RoleId { get; private set; }
+    }
+}
