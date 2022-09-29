5d0e6c1e21756d1dff82739b2d41fe1f0ce6c11d
P3truts <obreja.petrut@gmail.com>
2022-09-29 18:07:38 +0300

    Database: Base model - del constr, change Id

--- IKnowAGuy/Models/Base.cs
+++ IKnowAGuy/Models/Base.cs
@@ -2,15 +2,8 @@
 {
     public abstract class Base
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-
-        protected Base(int id, string name, string description)
-        {
-            Id = id;
-            Name = name;
-            Description = description;
-        }
+        public string? Id { get; private set; }
+        public string? Name { get; private set; }
+        public string? Description { get; private set; }
     }
 }
