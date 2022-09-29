f046134fa002975b7ee5b07fc79967d530a2d2c3
P3truts <obreja.petrut@gmail.com>
2022-09-29 10:58:04 +0300

    Models: add Base class

---
+++ IKnowAGuy/Models/Base.cs
@@ -0,0 +1,16 @@
+﻿namespace IKnowAGuy.Models
+{
+    public abstract class Base
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        protected Base(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}
