be0fe7a6b7769716174406718c7bfbbfe29588e0
P3truts <obreja.petrut@gmail.com>
2022-09-29 18:01:37 +0300

    Database: add DatabaseContext class

---
+++ IKnowAGuy/Data/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using IKnowAGuy.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace IKnowAGuy.Data
+{
+    public class DatabaseContext : IdentityDbContext<AppUser>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Ad> Ads { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
