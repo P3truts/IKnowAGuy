a06b1e05bf81fa1cf2191d49fe99e404120a5ca3
P3truts <obreja.petrut@gmail.com>
2022-09-29 11:10:30 +0300

    Models: add Job model

---
+++ IKnowAGuy/Models/Job.cs
@@ -0,0 +1,11 @@
+﻿namespace IKnowAGuy.Models
+{
+    public class Job : Base
+    {
+        public int ServiceId { get; private set; }
+        public Job(int id, string name, string description, int serviceId) : base(id, name, description)
+        {
+            ServiceId = serviceId;
+        }
+    }
+}
