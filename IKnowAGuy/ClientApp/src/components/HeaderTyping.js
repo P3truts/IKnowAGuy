fc0073a7bbe54595af7c550886ce76e44266668d
Octavian Dinu <dinuoctavianstefan@gmail.com>
2022-10-14 00:17:01 +0300

    Add HeaderTyping component

---
+++ IKnowAGuy/ClientApp/src/components/HeaderTyping.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './HeaderTyping.css';
+import Typed from 'typed.js';
+
+export const HeaderTyping = () => {
+    return (
+        <>
+            <div id='typed-strings'>
+                <h1>
+                    Find Handymans for <span id='typed'></span>
+                </h1>
+            </div>
+        </>
+    );
+};
+
+window.onload = function () {
+    new Typed('#typed', {
+        strings: ['paintings', 'renovations', 'cleaning', 'almost anything'],
+        typeSpeed: 90,
+        backSpeed: 50,
+        loop: true,
+        showCursor: false,
+    });
+};
