def65d6b10f10c7e86f77d3b07f8af95aa9c3815
Octavian Dinu <dinuoctavianstefan@gmail.com>
2022-10-14 00:16:21 +0300

    Add Ad components

--- IKnowAGuy/ClientApp/src/components/Ad.js
+++ IKnowAGuy/ClientApp/src/components/Ad.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './Ad.css';
 
-export const Ad = ({ ad }) => {
+export const Ad = ({ ad, index }) => {
+    const imagePath = 'Assets/handyman-' + index + '.png';
+    console.log(imagePath);
     return (
         <>
-            <div className='card'>
+            <div className='card ad-card'>
+                <img src={`Assets/handyman-${index}.png`} className='card-img-top' alt='...' />
                 <div className='card-body'>
                     <h5 className='card-title'>{ad.name}</h5>
                     <p className='card-text'>{ad.description}</p>
                     <p className='card-text'>{ad.date}</p>
-                    <p className='card-text'>{ad.jobCategory.Name}</p>
+                    <p className='card-text'>{ad.jobCategory.name}</p>
+                    <p className='card-text'>{ad.service.name}</p>
                     <a href='#' className='btn btn-primary'>
                         Contact
                     </a>
