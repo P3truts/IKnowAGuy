c4c139534eddaff70d23e9ab600379e196f5ac77
Octavian Dinu <dinuoctavianstefan@gmail.com>
2022-10-14 00:17:47 +0300

    Add SearchBar Component

---
+++ IKnowAGuy/ClientApp/src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SearchBar.css';
+
+export const SearchBar = () => {
+    return (
+        <div class='input-group input-group-lg search-bar'>
+            <input
+                type='text'
+                class='form-control'
+                aria-label='Sizing example input'
+                aria-describedby='inputGroup-sizing-lg'
+            />
+            <button type='button' class='btn btn-info'>
+                Search
+            </button>
+        </div>
+    );
+};
