94296c0af385185cf0ac0b2669d981f0f31d8e84
P3truts <obreja.petrut@gmail.com>
2022-10-26 15:14:50 +0300

    add Search function (on KeyPress)

--- IKnowAGuy/ClientApp/src/components/SearchBar.js
+++ IKnowAGuy/ClientApp/src/components/SearchBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../css/SearchBar.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 export const SearchBar = ({ setAds }) => {
     const [searchTerm, setSearchTerm] = useState('');
 
     const searchAds = async (searchTerm) => {
+        console.log('This is the search term: ', searchTerm);
         const req = await fetch(`ads/search/${(searchTerm)}`)
         if (req.ok) {
             var searchData = await req.json();
@@ -16,6 +17,13 @@ export const SearchBar = ({ setAds }) => {
             console.log("Request is nok");
             setAds([]);
         }
+    };
+
+    const handleKeypress = e => {
+      if (e.key === 'Enter') {
+        console.log('This is the search term from key down: ', searchTerm);
+        searchAds(searchTerm);
+      }
     };
 
     return (
@@ -28,6 +36,7 @@ export const SearchBar = ({ setAds }) => {
                 placeholder='Search for ads'
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
+                onKeyPress={handleKeypress}
             />
             <button type='button' className='btn btn-dark' onClick={() => searchAds(searchTerm)}>
                 Search
