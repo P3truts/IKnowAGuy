f311c010db03a29c68dfe849a70e6507d8fc358c
P3truts <obreja.petrut@gmail.com>
2022-10-27 16:33:28 +0300

    add Update+Delete buttons

--- IKnowAGuy/ClientApp/src/pages/AdDetails.js
+++ IKnowAGuy/ClientApp/src/pages/AdDetails.js
@@ -21,6 +21,30 @@ export function AdDetails() {
         }
     };
 
+    const deleteAd = async () => {
+        const req = await fetch(`ads/${id}`);
+        // const req = await fetch(`ads/${1}`);
+        if (req.ok) {
+            const res = await req.json();
+            setAd(res);
+        } else {
+            console.log('req is not ok');
+            setAd([]);
+        }
+    };
+
+    const updateAd = async () => {
+        const req = await fetch(`ads/${id}`);
+        // const req = await fetch(`ads/${1}`);
+        if (req.ok) {
+            const res = await req.json();
+            setAd(res);
+        } else {
+            console.log('req is not ok');
+            setAd([]);
+        }
+    };
+
     useEffect(() => {
         loader();
     }, []);
@@ -30,7 +54,14 @@ export function AdDetails() {
             
             {ad.id && (
             <div className='container' style={{ 'paddingLeft' : '20%' }}>
-                <h2>Ad Details</h2>
+                <h2>Ad Details
+                    <button type='button' className='btn btn-info' style={{ 'position' : 'relative', 'left' : '38%' }} onClick={() => updateAd(ad.id)}>
+                        Update
+                    </button>
+                    <button type='button' className='btn btn-danger' style={{ 'position' : 'relative', 'left' : '38%' }} onClick={() => deleteAd(ad.id)}>
+                        Delete
+                    </button>
+                </h2>
                 <img src={`Assets/handyman-${ad.id}.png`} style={{ 'width' : '70%' }} alt='...' />
                 <div className='card-body'>
                     <h3>{ad.name}</h3>
