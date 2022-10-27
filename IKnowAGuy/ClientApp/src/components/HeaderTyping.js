aa71f3f1dd21bdde2bdcf7405c84375eb4a44652
P3truts <obreja.petrut@gmail.com>
2022-10-27 23:05:23 +0300

    adapt typed.js for react load

--- IKnowAGuy/ClientApp/src/components/HeaderTyping.js
+++ IKnowAGuy/ClientApp/src/components/HeaderTyping.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import '../css/HeaderTyping.css';
 import Typed from 'typed.js';
+import { useEffect } from 'react';
+
 
 export const HeaderTyping = () => {
+
+    const typed =() => {
+        new Typed('#typed', {
+            strings: ['paintings', 'renovations', 'cleaning', 'almost anything'],
+            typeSpeed: 90,
+            backSpeed: 9,
+            loop: true,
+            showCursor: false,
+        });
+    };
+
+    useEffect(() => {
+        typed();
+      });
+
     return (
         <>
             <div id='typed-strings'>
@@ -12,14 +29,4 @@ export const HeaderTyping = () => {
             </div>
         </>
     );
-};
-
-window.onload = function () {
-    new Typed('#typed', {
-        strings: ['paintings', 'renovations', 'cleaning', 'almost anything'],
-        typeSpeed: 90,
-        backSpeed: 50,
-        loop: true,
-        showCursor: false,
-    });
 };
