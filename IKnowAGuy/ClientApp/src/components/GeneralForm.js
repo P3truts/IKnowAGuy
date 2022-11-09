7bcc8358b93ad721bda9a8ae37eaebee30d5e0bb
Octavian Dinu <dinuoctavianstefan@gmail.com>
2022-11-09 12:02:38 +0200

    Add fill update ad form with ad details functionality

--- IKnowAGuy/ClientApp/src/components/GeneralForm.js
+++ IKnowAGuy/ClientApp/src/components/GeneralForm.js
@@ -1,73 +1,74 @@
-import "../css/AdForm.css";
+import '../css/AdForm.css';
 
 const GeneralForm = ({
     onSubmit,
     onJobTypeChange,
-    jobType = "Choose category",
+    jobType = 'Choose category',
     onServiceChange,
-    service = "Choose a service",
+    service = 'Choose a service',
     onCountyChange,
-    county = "Ilfov",
+    county = 'Ilfov',
     counties,
     onCityChange,
-    city = "Bucuresti",
+    city = 'Bucuresti',
     cities,
     onAdNameChange,
     adName,
     onDescriptionChange,
     description,
-    isPending
+    isPending,
 }) => {
     return (
-        <form className="form-ad" onSubmit={onSubmit}>
-            <div className="mb-3">
-                <label htmlFor="job-type" className="form-label">
+        <form className='form-ad' onSubmit={onSubmit}>
+            <div className='mb-3'>
+                <label htmlFor='job-type' className='form-label'>
                     Job Type
                 </label>
                 <select
                     onChange={onJobTypeChange}
-                    id="jobType"
-                    className="form-select"
-                    name="job-type"
-                    aria-label="Default select example"
-                    defaultValue={jobType}>
-                    <option value="Piping">Piping</option>
-                    <option value="Electrics">Electrics</option>
-                    <option value="Carpentry">Carpentry</option>
+                    id='jobType'
+                    className='form-select'
+                    name='job-type'
+                    aria-label='Default select example'
+                    value={jobType}
+                >
+                    <option value='Piping'>Piping</option>
+                    <option value='Electrics'>Electrics</option>
+                    <option value='Carpentry'>Carpentry</option>
                 </select>
             </div>
 
-            <div className="mb-3">
-                <label htmlFor="service" className="form-label">
+            <div className='mb-3'>
+                <label htmlFor='service' className='form-label'>
                     Service
                 </label>
                 <select
                     onChange={onServiceChange}
-                    className="form-select"
-                    id="service"
-                    name="service"
-                    aria-label="Default select example"
-                    defaultValue={service}>
-                    <option value="Pipe repairs">Pipe repairs</option>
-                    <option value="Electric installation">
-                        Electric installation
-                    </option>
-                    <option value="General repairs">General repairs</option>
+                    className='form-select'
+                    id='service'
+                    name='service'
+                    aria-label='Default select example'
+                    value={service}
+                >
+                    <option value='Pipe repairs'>Pipe repairs</option>
+                    <option value='Electric installation'>Electric installation</option>
+                    <option value='General repairs'>General repairs</option>
                 </select>
             </div>
-            <div className="mb-service3">
-                <label htmlFor="county" className="form-label">
+            <div className='mb-service3'>
+                <label htmlFor='county' className='form-label'>
                     County
                 </label>
 
                 <select
                     onChange={onCountyChange}
-                    className="form-select"
-                    id="county"
-                    name="county"
-                    aria-label="Default select example"
-                    defaultValue={county}>
-                    {counties.length > 1 &&
+                    className='form-select'
+                    id='county'
+                    name='county'
+                    aria-label='Default select example'
+                    value={county}
+                >
+                    {counties.length > 0 &&
                         counties.map((county, index) => (
                             <option key={index} value={county.nume}>
                                 {county.nume}
@@ -75,18 +76,19 @@ const GeneralForm = ({
                         ))}
                 </select>
             </div>
-            <div className="mb-3">
-                <label htmlFor="city" className="form-label">
+            <div className='mb-3'>
+                <label htmlFor='city' className='form-label'>
                     City
                 </label>
                 <select
                     onChange={onCityChange}
-                    className="form-select"
-                    id="city"
-                    name="city"
-                    aria-label="Default select example"
-                    defaultValue={city}>
-                    {cities.length > 1 &&
+                    className='form-select'
+                    id='city'
+                    name='city'
+                    aria-label='Default select example'
+                    value={city}
+                >
+                    {cities.length > 0 &&
                         cities.map((city, index) => (
                             <option key={index} value={city.nume}>
                                 {city.nume}
@@ -94,45 +96,45 @@ const GeneralForm = ({
                         ))}
                 </select>
             </div>
-            <div className="mb-3">
-                <label htmlFor="ad-name" className="form-label">
+            <div className='mb-3'>
+                <label htmlFor='ad-name' className='form-label'>
                     Ad Name
                 </label>
                 <input
                     onChange={onAdNameChange}
-                    className="form-control"
-                    id="name"
-                    name="ad-name"
-                    type="text"
-                    aria-label="default input example"
+                    className='form-control'
+                    id='name'
+                    name='ad-name'
+                    type='text'
+                    aria-label='default input example'
                     value={adName}
                     required
                 />
             </div>
 
-            <div className="mb-3">
-                <div className="mb-3">
-                    <label htmlFor="description" className="form-label">
+            <div className='mb-3'>
+                <div className='mb-3'>
+                    <label htmlFor='description' className='form-label'>
                         Ad Description
                     </label>
                     <textarea
                         onChange={onDescriptionChange}
-                        className="form-control"
-                        id="description"
-                        name="description"
-                        rows="3"
-                        required>
-                        {description}
-                    </textarea>
+                        className='form-control'
+                        id='description'
+                        name='description'
+                        rows='3'
+                        defaultValue={description}
+                        required
+                    ></textarea>
                 </div>
             </div>
 
             {!isPending ? (
-                <button type="submit" className="btn">
+                <button type='submit' className='btn'>
                     Submit
                 </button>
             ) : (
-                <button disabled type="submit" className="btn">
+                <button disabled type='submit' className='btn'>
                     Adding Add...
                 </button>
             )}
--- IKnowAGuy/ClientApp/src/pages/UpdateAd.js
+++ IKnowAGuy/ClientApp/src/pages/UpdateAd.js
@@ -2,14 +2,76 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import fetchapi from '../utils/fetchApi';
 import FETCH_URL from '../AppFetchUrl';
+import GeneralForm from '../components/GeneralForm';
 
 const UpdateAd = () => {
     const { id } = useParams();
-    const [ad, setAd] = useState({});
+    const [ad, setAd] = useState({
+        address: { county: '', city: '' },
+        description: '',
+        jobCategory: { services: '', name: '', description: '' },
+        name: '',
+        service: { name: '', description: '' },
+    });
 
     useEffect(() => {
-        fetchapi.get(`${FETCH_URL.ads}/${id}`).then((ad) => console.log(ad));
-    });
+        fetchapi.get(`${FETCH_URL.ads}/${id}`).then((ad) => {
+            setAd(ad);
+            console.log(ad);
+        });
+    }, []);
+    console.log(ad.address.county);
+
+    const handleSubmit = () => {
+        console.log('submit');
+    };
+
+    const onJobTypeChange = () => {
+        console.log('on job type change');
+    };
+
+    const onServiceChange = () => {
+        console.log('service change');
+    };
+    const onCountyChange = () => {
+        console.log('service change');
+    };
+
+    const cityChange = () => {
+        console.log('service change');
+    };
+
+    const adNameChange = () => {
+        console.log('ad name change');
+    };
+
+    const changeDescription = () => {
+        console.log('description');
+    };
+
+    return (
+        <GeneralForm
+            onSubmit={handleSubmit}
+            onJobTypeChange={onJobTypeChange}
+            jobType={ad.jobCategory.name}
+            onServiceChange={onServiceChange}
+            service={ad.service.name}
+            onCountyChange={onCountyChange}
+            county={ad.address.county}
+            counties={[{ nume: ad.address.county }]}
+            onCityChange={cityChange}
+            city={ad.address.city}
+            cities={[{ nume: ad.address.city }]}
+            onAdNameChange={adNameChange}
+            adName={ad.name}
+            onDescriptionChange={(e) => {
+                const newAd = { ...ad };
+                newAd.description = e.target.value;
+            }}
+            description={ad.description}
+            isPending={true}
+        />
+    );
 };
 
 export default UpdateAd;
