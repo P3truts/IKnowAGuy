c0957496905f697ad7ceb96c259688c8f38f8fb6
P3truts <obreja.petrut@gmail.com>
2022-11-10 12:27:07 +0200

    LoginUser: add Jwt token creation and authentication

---
+++ IKnowAGuy/Services/Implementation/AuthManager.cs
@@ -0,0 +1,79 @@
+﻿using IKnowAGuy.Models;
+using IKnowAGuy.ModelsDTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IKnowAGuy.Services.Implementation
+{
+    public class AuthManager : IAuthManager
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IConfiguration _configuration;
+        private AppUser _user;
+
+        public AuthManager(UserManager<AppUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<string> CreateToken()
+        {
+            var signinCredentials = GetSigningCredentials();
+            var claims = await GetClaims();
+            var token = GenerateTokenOptions(signinCredentials, claims);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signinCredentials, List<Claim> claims)
+        {
+            var jwtSettings = _configuration.GetSection("Jwt");
+            var expiration = DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings.GetSection("lifetime").Value));
+
+            var token = new JwtSecurityToken
+            (
+                issuer: jwtSettings.GetSection("validIssuer").Value,
+                claims: claims,
+                expires: expiration,
+                signingCredentials: signinCredentials
+            );
+
+            return token;
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, _user.UserName)
+            };
+
+            var roles = await _userManager.GetRolesAsync(_user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = Environment.GetEnvironmentVariable("KEY", EnvironmentVariableTarget.Machine);
+            var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+
+        public async Task<bool> ValidateUser(LoginUserDTO loginUser)
+        {
+            _user = await _userManager.FindByEmailAsync(loginUser.Email);
+            return (_user != null && await _userManager.CheckPasswordAsync(_user, loginUser.Password));
+
+        }
+    }
+}
