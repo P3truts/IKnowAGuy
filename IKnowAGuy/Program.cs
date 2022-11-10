c3c9c6cbf82431301ef2bda037dd28249587e349
P3truts <obreja.petrut@gmail.com>
2022-11-10 12:25:35 +0200

    LoginUser: add Jwt Token services

--- IKnowAGuy/Program.cs
+++ IKnowAGuy/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 using IKnowAGuy.Repositories;
 using IKnowAGuy.Repositories.Implementation;
@@ -9,6 +10,9 @@ using IKnowAGuy.Models;
 using IKnowAGuy.Data.Seed;
 using IKnowAGuy.Services.Implementation;
 using IKnowAGuy.Configurations;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DatabaseContext");
@@ -19,10 +23,31 @@ builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(c
 
 builder.Services.AddAutoMapper(typeof(MapperInitializer));
 
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Environment.GetEnvironmentVariable("KEY");
+
 builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings.GetSection("Issuer").Value,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+    };
+});
+
+
 builder.Services.AddIdentity<AppUser, IdentityRole>(q => q.User.RequireUniqueEmail = true)
     .AddEntityFrameworkStores<DatabaseContext>().AddDefaultTokenProviders();
 
+builder.Services.AddAuthorization();
 builder.Services.AddSession();
 
 builder.Services.AddScoped<IAdService, AdService>();
@@ -30,18 +55,49 @@ builder.Services.AddScoped<IAdRepository, AdRepository>();
 builder.Services.AddScoped<IAddressRepository, AddressRepository>();
 builder.Services.AddScoped<IJobRepository, JobRepository>();
 builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IAuthManager, AuthManager>();
 
 builder.Services.AddSwaggerGen(options =>
-        options.SwaggerDoc("v1",
-        new Microsoft.OpenApi.Models.OpenApiInfo
         {
-            Title = "IKnowAGuy",
-            Description = "IKnowAGuy API",
-            Version = "v1",
-        }
-        )
+            options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Description = @"JWT Authorization Header using the Bearer scheme.
+                                Enter 'Bearer' [space] and then your token in the text input below.
+                                Example: 'Bearer 12345abcdef'",
+                Name = "Authorization",
+                In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
 
-    );
+            options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement() {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "0auth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header
+                    },
+                    new List<string>()
+                }
+            });
+
+            options.SwaggerDoc("v1",
+            new Microsoft.OpenApi.Models.OpenApiInfo
+            {
+                Title = "IKnowAGuy",
+                Description = "IKnowAGuy API",
+                Version = "v1",
+            });
+        });
+
+/*builder.Services.AddControllers().AddNewtonsoftJson(opt => 
+    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);*/
 
 var app = builder.Build();
 
