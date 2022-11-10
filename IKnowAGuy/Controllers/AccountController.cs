33b8dc6cca4fa09679bb34bd2b5eb4d13285af1a
P3truts <obreja.petrut@gmail.com>
2022-11-10 12:29:03 +0200

    LoginUser: add Login method

--- IKnowAGuy/Controllers/AccountController.cs
+++ IKnowAGuy/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using IKnowAGuy.Models;
+using IKnowAGuy.ModelsDTO;
+using IKnowAGuy.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,15 +11,15 @@ namespace IKnowAGuy.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
-        private readonly SignInManager<AppUser> _signInManager;
         private readonly ILogger<AccountController> _logger;
         private readonly IMapper _mapper;
+        private readonly IAuthManager _authManager;
 
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ILogger<AccountController> logger,
-            IMapper mapper)
+        public AccountController(UserManager<AppUser> userManager, ILogger<AccountController> logger, IMapper mapper, 
+            IAuthManager authManager)
         {
             _userManager = userManager;
-            _signInManager = signInManager;
+            _authManager = authManager;
             _logger = logger;
             _mapper = mapper;
         }
@@ -54,6 +56,32 @@ namespace IKnowAGuy.Controllers
             {
                 _logger.LogError(ex, $"Something went wrong in the {nameof(Register)}!");
                 return Problem($"Something went wrong in the {nameof(Register)}!", statusCode: 500);
+            }
+        }
+
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Login([FromBody] LoginUserDTO loginUser)
+        {
+            _logger.LogInformation($"Login attempt for {loginUser.Email}!");
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                if (!await _authManager.ValidateUser(loginUser))
+                {
+                    return Unauthorized();
+                }
+                return Accepted(new { Token = await _authManager.CreateToken() });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the {nameof(Login)}!");
+                return Problem($"Something went wrong in the {nameof(Login)}!", statusCode: 500);
             }
         }
     }
