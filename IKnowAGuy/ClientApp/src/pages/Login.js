75a77cefc463dc2e46efdc4ef6e1d8d4ced35eb7
P3truts <obreja.petrut@gmail.com>
2022-11-11 13:15:54 +0200

    LoginUser: add state parameters

--- IKnowAGuy/ClientApp/src/pages/Login.js
+++ IKnowAGuy/ClientApp/src/pages/Login.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import PATH from '../AppPaths';
+import fetchapi from '../utils/fetchApi';
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    // const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submit = async (e) => {
+        e.preventDefault();
+
+        fetchapi.login("https://localhost:44497/account/login", {
+            email,
+            password
+        }).then(() => {
+            navigate(PATH.Home);
+        });
+    }
+
     return (
-        <div class="mb-3" style={{ padding: '15%' }}>
-            <form>
+        <div className="mb-3" style={{ padding: '15%' }}>
+            <form onSubmit={submit}>
                 <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
-                <input type='email' className='form-control' placeholder='Email address' required/>
-                <input type='password' className='form-control' placeholder='Password' required/>
+                <input type='email' className='form-control' placeholder='Email address' required
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <input type='password' className='form-control' placeholder='Password' required
+                    onChange={e => setPassword(e.target.value)}
+                />
                 <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign in</button>
             </form>
         </div>
