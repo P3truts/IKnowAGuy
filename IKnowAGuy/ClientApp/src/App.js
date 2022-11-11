aad221b25586ad221131ac968fa0e53ad2aad215
P3truts <obreja.petrut@gmail.com>
2022-11-11 13:14:32 +0200

    AutheticatedUser: add fetchUser

--- IKnowAGuy/ClientApp/src/App.js
+++ IKnowAGuy/ClientApp/src/App.js
@@ -1,14 +1,34 @@
 import { Route, Routes } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import Layout from "./components/Layout";
+import React, {useState, useEffect} from 'react';
 
 const App = () => {
+    const [username, setUsername] = useState('');
+
+    useEffect(() => {
+        (
+        async () => {
+            const response = await fetch('https://localhost:44497/account/user', {
+                headers: {'Content-Type': 'application/json'},
+                credentials: 'include'
+        });
+            // }).then((res) => response.json()).then((content) => setUsername(content));
+
+            const content = await response.text();
+            setUsername(content);
+
+            console.log("app username", username);
+        }
+        )()
+    }, []);
+
     return (
-        <Layout>
+        <Layout username={username}>
             <Routes>
                 {AppRoutes.map((route, index) => {
                     const { element, ...rest } = route;
-                    return <Route key={index} {...rest} element={element} />;
+                    return <Route key={index} {...rest} element={element}/>;
                 })}
             </Routes>
         </Layout>
